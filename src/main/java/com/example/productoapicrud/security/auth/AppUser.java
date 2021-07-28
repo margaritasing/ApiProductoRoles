59a9d070e679cc85417108dd1a7fab479057680a
margaritasing <61855440+margaritasing@users.noreply.github.com>
2021-07-27 22:07:30 -0300

    Agrenado el grantedAuthority"

--- src/main/java/com/example/productoapicrud/security/auth/AppUser.java
+++ src/main/java/com/example/productoapicrud/security/auth/AppUser.java
@@ -2,22 +2,23 @@ package com.example.productoapicrud.security.auth;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.Collection;
 
 @Entity
-@Table(name = "users")
+@Table(name="users")
 public class AppUser implements UserDetails {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer user_id;
     private String username;
     private String password;
     @JsonBackReference
-    @ManyToMany(cascade = CascadeType.DETACH)
+    @ManyToMany(cascade = CascadeType.DETACH, fetch = FetchType.EAGER)
     @JoinTable(
-            name = "user_x_authorities",
+            name = "users_x_authorities",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "authority_id"))
     private Collection<MiSimpleGrantedAuthority> authorities;
@@ -28,7 +29,6 @@ public class AppUser implements UserDetails {
 
     public AppUser() {
     }
-
     public AppUser(Integer user_id, String username, String password, Collection<MiSimpleGrantedAuthority> authorities, boolean accountNonExpired, boolean accountNonLocked, boolean enabled, boolean credentialsNonExpired) {
         this.user_id = user_id;
         this.username = username;
@@ -40,39 +40,87 @@ public class AppUser implements UserDetails {
         this.credentialsNonExpired = credentialsNonExpired;
     }
 
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+    public Integer getUser_id() {
+        return user_id;
     }
 
-    @Override
-    public String getPassword() {
-        return null;
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Collection<MiSimpleGrantedAuthority> authorities) {
+        this.authorities = authorities;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return accountNonExpired;
+    }
+
+    public void setAccountNonExpired(boolean accountNonExpired) {
+        this.accountNonExpired = accountNonExpired;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return accountNonLocked;
     }
 
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
+    public void setAccountNonLocked(boolean accountNonLocked) {
+        this.accountNonLocked = accountNonLocked;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+        this.credentialsNonExpired = credentialsNonExpired;
+    }
+
+    @Override
+    public String toString() {
+        return "AppUser{" +
+                "user_id=" + user_id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", accountNonExpired=" + accountNonExpired +
+                ", accountNonLocked=" + accountNonLocked +
+                ", enabled=" + enabled +
+                ", credentialsNonExpired=" + credentialsNonExpired +
+                '}';
     }
 }
--- src/main/java/com/example/productoapicrud/security/auth/MiSimpleGrantedAuthority.java
+++ src/main/java/com/example/productoapicrud/security/auth/MiSimpleGrantedAuthority.java
@@ -1,4 +1,60 @@
 package com.example.productoapicrud.security.auth;
 
-public class MiSimpleGrantedAuthority {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.jackson2.SimpleGrantedAuthorityMixin;
+import org.springframework.util.Assert;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Set;
+
+@Entity
+@Table(name="authorities")
+public class MiSimpleGrantedAuthority implements GrantedAuthority {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer authority_id;
+    @Column(nullable = false)
+    private String role;
+    @ManyToMany(mappedBy = "authorities", fetch = FetchType.LAZY)
+    private Set<AppUser> usuarios;
+
+    public MiSimpleGrantedAuthority(String role) {
+        this.role = role;
+    }
+    public MiSimpleGrantedAuthority() {
+    }
+
+    public String getAuthority() {
+        return this.role;
+    }
+
+    public String toString() {
+        return this.role;
+    }
+
+    public void setAuthority_id(Integer authority_id) {
+        this.authority_id = authority_id;
+    }
+
+    public Integer getAuthority_id() {
+        return authority_id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Set<AppUser> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(Set<AppUser> usuarios) {
+        this.usuarios = usuarios;
+    }
 }
